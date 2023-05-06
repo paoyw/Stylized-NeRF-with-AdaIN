4c31c34ad3fdb93d6638baa941f5564e7124196f
vppyw <vincent01pao@gmail.com>
2023-05-06 11:45:47 +0800

    Update gen_adain.py

--- gen_adain.py
+++ gen_adain.py
@@ -22,6 +22,7 @@ def main(args):
     std = [0.229, 0.224, 0.225]
     tfm = transforms.Compose([
             transforms.ToTensor(),
+            transforms.Resize((512, 512)),
             transforms.Normalize(mean=mean, std=std)
           ])
     content_img = Image.open(args.content_image).convert('RGB')
@@ -33,13 +34,36 @@ def main(args):
     with torch.no_grad():
         c_feats, s_feats, norm_feats, trans_img = model(
                                                     content_img,
-                                                    style_img
+                                                    style_img,
+                                                    return_hidden=True
                                                   )
         t_feats = model.enc(trans_img) 
+        rec_c_img = model.dec(c_feats)
+        rec_s_img = model.dec(s_feats)
         trans_img = utils.dataloader.denorm(
                         trans_img.squeeze().to('cpu')
                     )
-    save_image(trans_img, args.transfer_image)
+        rec_c_img = utils.dataloader.denorm(
+                        rec_c_img.squeeze().to('cpu')
+                    )
+        rec_s_img = utils.dataloader.denorm(
+                        rec_s_img.squeeze().to('cpu')
+                    )
+        cat_img = torch.cat(
+            [
+                utils.dataloader.denorm(
+                    content_img.squeeze().to('cpu'),
+                ),
+                utils.dataloader.denorm(
+                    style_img.squeeze().to('cpu'),
+                ),
+                trans_img,
+                rec_c_img,
+                rec_s_img,
+            ],
+            dim=2
+         )
+    save_image(cat_img, args.transfer_image)
 
 if __name__ == '__main__':
     args = parse_args()
